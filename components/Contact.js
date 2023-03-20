1ecc540a2a7de3637ec591c39cb0eb8deb176344
Robert McWhirter <rob@robmcw.com>
2023-03-20 11:45:58 +0100

    Add leave email form feature complete with success message

--- components/Contact.js
+++ components/Contact.js
@@ -3,14 +3,14 @@ import { sendContactForm } from "../lib/api"
 
 const Contact = () => {
 
-    const initValues = { email: " " }
-
+    const initValues = { email: null }
     const initState = { values: initValues }
-
     const [state, setState] = useState(initState)
-
     const { values } = state
+    const [emailSent, setEmailSent] = useState(false)
 
+
+    // Update state based on changes to form
     const handleChange = ({ target }) => setState((prev) => ({
         ...prev,
         values: {
@@ -19,30 +19,59 @@ const Contact = () => {
         }
     }))
 
-    const onSubmit = async () => {
+    // On submit, update state and use Nodemailer to send email with field input as contents
+    const onSubmit = async (event) => {
         setState((prev) => ({
             ...prev,
         }))
-        await sendContactForm(values)
+
+        try {
+            // Prevent page refresh so emailSentBlock shows
+            event.preventDefault()
+
+            await sendContactForm(values)
+            setState(initState)
+            setEmailSent(true)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const submitBtn =
+        <div className="flexContainer">
+            <button
+                className="button-sm"
+                disabled={!values.email}
+                onClick={onSubmit}
+            > Send
+            </button>
+        </div>
+
+    // On form submission, show success message
+    let emailSentBlock = ""
+    if (emailSent) {
+        emailSentBlock = <p>✅ Sent </p>
     }
 
     return (
-        <form >
-            <label >Your email</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                required
-            />
-            <button
-                disabled={!values.email}
-                onClick={onSubmit}
-            >Submit
-            </button>
+
+        <form action={setEmailSent} method="post">
+            <div className="flexContainer">
+                <label >Leave your email for when the next questions drop: </label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="youremail@email.com"
+                    value={values.email}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            {submitBtn}
+            {emailSentBlock}
         </form>
+
     )
 }
 
